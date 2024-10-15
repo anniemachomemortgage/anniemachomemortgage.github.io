3953baaa3d820dc112a1ba1775b810dc2988509b
Nick Hopkins <nhopkins@annie-mac.com>
2024-10-15 11:39:04 -0400

    add

---
+++ internal/snapform/script.js
@@ -0,0 +1,24 @@
+var currentQuestion = 0;
+showQuestion(currentQuestion);
+function showQuestion(n) {
+    var x = document.getElementsByClassName("tab");
+    x[n].classList.add("tab-display");
+    trackingProgressMeter(n)
+}
+function questionToggle(n) {
+    let currentActiveTargetName = event.target.name;
+    currentQuestion = currentQuestion + n;
+    showQuestion(currentQuestion);
+    window.scrollTo(0,document.body.scrollHeight);
+    var i, x = document.getElementsByName(currentActiveTargetName);
+    for (i = 0; i < x.length; i++) {
+        x[i].removeAttribute('onclick')
+    }
+}
+function trackingProgressMeter(n) {
+    var i, x = document.getElementsByClassName("step");
+    for (i = 0; i < x.length; i++) {
+        x[i].className = x[i].className.replace("indicator", "prevind");
+    }
+    x[n-1].classList.add("indicator");
+}
